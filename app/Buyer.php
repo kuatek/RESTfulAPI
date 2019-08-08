313e66322fbe1381e1810faba7f05a3504f74e6f
kuatek <kuatekevin@gmail.com>
2019-08-08 12:38:30 +0100

    Initial commit

---
+++ app/Buyer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Buyer extends User
+{
+    public function transaction(){
+
+        return $this->hasMany(Transaction::class);
+    }
+
+
+}
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Category;
+use App\Product;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+        'verified'=> $verified = $faker->randomElement([User::VERIFIED_USER,User::UNVERIFIED_USER]),
+        'verification_token'=> $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin  '=>$faker->randomElement([User::ADMIN_USER,User::REGULAR_USER]),
+
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' =>$faker->paragraph(1)
+
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' =>$faker->paragraph(1),
+        'quantity'=>$faker->numberBetween(1,10),
+        'status'=>$faker->randomElement([Product::AVAILABLE_PRODUCT,Product::UNAVAILABLE_PRODUCT]),
+        'image'=>$faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id'=>User::all()->random()->id,
+        User::inR
+
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' =>$faker->paragraph(1),
+        'quantity'=>$faker->numberBetween(1,10),
+        'status'=>$faker->randomElement([Product::AVAILABLE_PRODUCT,Product::UNAVAILABLE_PRODUCT]),
+        'image'=>$faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id'=>User::all()->random()->id,
+        User::inR
+
+    ];
+});
